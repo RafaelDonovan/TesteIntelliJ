a97069e13537e2c37727a7519055317009d62416
Rafael Donovan <rafaael.barbosa96@gmail.com>
2023-09-27 21:30:38 -0300

    Criado arquivo

---
+++ src/main/java/br/com/carstore/servlet/CreateCarServlet.java
@@ -0,0 +1,26 @@
+package br.com.carstore.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/create-car")
+public class CreateCarServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String carName = request.getParameter("carName");
+
+        System.out.println(carName);
+
+       // Car car = new Car(carName);
+
+      //  new CarDao().createCar(car);
+
+        request.getRequestDispatcher("index.html").forward(request, response);
+    }
+}
